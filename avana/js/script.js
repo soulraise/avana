a6a09b02fe18fb65e3a4529344ce0d6548a7427c
stavrdriff <stavr.driff@gmail.com>
2021-07-08 20:44:43 +0300

    headerJs+ leftSideOfPurchaseSection

--- avana/js/script.js
+++ avana/js/script.js
@@ -11,7 +11,23 @@ $(document).ready(function () {
       fade: true,
       cssEase: 'linear'
    });
-});;
+});
+
+
+function headerHidden() {
+   let posWindow = 0;
+   window.addEventListener('scroll', function () {
+      let pos = document.getElementById('main').getBoundingClientRect();
+      let posWindow2 = pos.y;
+      if (posWindow < posWindow2) {
+         document.querySelector('.header').classList.remove('header__fixed')
+      } else {
+         document.querySelector('.header').classList.add('header__fixed')
+      }
+      posWindow = posWindow2;
+   })
+}
+headerHidden();;
 
 function testWebP(callback) {
 
