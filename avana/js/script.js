59d7ab762655fdd1664bda4b283adf2f4ad669b8
stavrdriff <stavr.driff@gmail.com>
2021-07-08 13:09:41 +0300

    header+ slider+ first section+

--- avana/js/script.js
+++ avana/js/script.js
@@ -1,4 +1,17 @@
-alert('Hello world');;
+// alert('Hello world');
+
+$(document).ready(function () {
+   $('.main__slider').slick({
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      fade: true,
+      cssEase: 'linear'
+   });
+});;
 
 function testWebP(callback) {
 
