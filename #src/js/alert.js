c18a547aae1451837073f5a2d84635acc9302920
stavrdriff <stavr.driff@gmail.com>
2021-07-13 15:32:29 +0300

    added favicon, started cart page

--- #src/js/alert.js
+++ #src/js/alert.js
@@ -34,30 +34,22 @@ $(document).ready(function () {
       dots: false,
       infinite: true,
       arrows: true,
-      speed: 600,
+      speed: 500,
       slidesToShow: 3,
-      slidesToScroll: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
       responsive: [
          {
-            breakpoint: 1024,
-            settings: {
-               slidesToShow: 3,
-               slidesToScroll: 3,
-
-            }
-         },
-         {
-            breakpoint: 600,
+            breakpoint: 800,
             settings: {
                slidesToShow: 2,
-               slidesToScroll: 2
             }
          },
          {
-            breakpoint: 480,
+            breakpoint: 500,
             settings: {
                slidesToShow: 1,
-               slidesToScroll: 1
             }
          }
       ]
