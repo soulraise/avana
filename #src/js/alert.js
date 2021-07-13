7b16aa48242408809ed9151558da45460f5d56d9
Anton Rudenko <rudenko.a84@gmail.com>
2021-07-13 18:51:26 +0300

    add modal section

--- #src/js/alert.js
+++ #src/js/alert.js
@@ -1,57 +1,31 @@
 // alert('Hello world');
 
 $(document).ready(function () {
-   $('.main__slider').slick({
-      dots: true,
-      arrows: false,
-      infinite: true,
-      speed: 500,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      fade: true,
-      cssEase: 'linear'
-   });
+	$('.main__slider').slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		speed: 500,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		fade: true,
+		cssEase: 'linear'
+	});
 });
 
 
 function headerHidden() {
-   let posWindow = 0;
-   window.addEventListener('scroll', function () {
-      let pos = document.getElementById('anchor').getBoundingClientRect();
-      let posWindow2 = pos.y;
-      if (posWindow < posWindow2) {
-         document.querySelector('.header').classList.remove('header__fixed')
-      } else {
-         document.querySelector('.header').classList.add('header__fixed')
-      }
-      posWindow = posWindow2;
-   })
+	let posWindow = 0;
+	window.addEventListener('scroll', function () {
+		let pos = document.getElementById('anchor').getBoundingClientRect();
+		let posWindow2 = pos.y;
+		if (posWindow < posWindow2) {
+			document.querySelector('.header').classList.remove('header__fixed')
+		} else {
+			document.querySelector('.header').classList.add('header__fixed')
+		}
+		posWindow = posWindow2;
+	})
 }
 headerHidden();
 
-$(document).ready(function () {
-   $('.related__items').slick({
-      dots: false,
-      infinite: true,
-      arrows: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      responsive: [
-         {
-            breakpoint: 800,
-            settings: {
-               slidesToShow: 2,
-            }
-         },
-         {
-            breakpoint: 500,
-            settings: {
-               slidesToShow: 1,
-            }
-         }
-      ]
-   });
-});
--- avana/js/script.js
+++ avana/js/script.js
@@ -1,60 +1,35 @@
 // alert('Hello world');
 
 $(document).ready(function () {
-   $('.main__slider').slick({
-      dots: true,
-      arrows: false,
-      infinite: true,
-      speed: 500,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      fade: true,
-      cssEase: 'linear'
-   });
+	$('.main__slider').slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		speed: 500,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		fade: true,
+		cssEase: 'linear'
+	});
 });
 
 
 function headerHidden() {
-   let posWindow = 0;
-   window.addEventListener('scroll', function () {
-      let pos = document.getElementById('anchor').getBoundingClientRect();
-      let posWindow2 = pos.y;
-      if (posWindow < posWindow2) {
-         document.querySelector('.header').classList.remove('header__fixed')
-      } else {
-         document.querySelector('.header').classList.add('header__fixed')
-      }
-      posWindow = posWindow2;
-   })
+	let posWindow = 0;
+	window.addEventListener('scroll', function () {
+		let pos = document.getElementById('anchor').getBoundingClientRect();
+		let posWindow2 = pos.y;
+		if (posWindow < posWindow2) {
+			document.querySelector('.header').classList.remove('header__fixed')
+		} else {
+			document.querySelector('.header').classList.add('header__fixed')
+		}
+		posWindow = posWindow2;
+	})
 }
 headerHidden();
 
-$(document).ready(function () {
-   $('.related__items').slick({
-      dots: false,
-      infinite: true,
-      arrows: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      responsive: [
-         {
-            breakpoint: 800,
-            settings: {
-               slidesToShow: 2,
-            }
-         },
-         {
-            breakpoint: 500,
-            settings: {
-               slidesToShow: 1,
-            }
-         }
-      ]
-   });
-});;
+;
 new Vue({
 	el: '#app',
 	data: {
@@ -62,25 +37,31 @@ new Vue({
 			{
 				title: 'Помпа BlueRain',
 				price: 130,
+				rent: 20,
 				img: 'img/HandPomp.png',
-				label: 'mini'
+				label: 'mini',
+				discription: 'Помпа – то механический насосос, надеваемый на горлышко бутылки. Закрепляется после сняти крышки. Не меняет температуру воды.',
 			},
 			{
 				title: 'Помпа BlueRain2',
 				price: 131,
 				img: 'img/HandPomp.png',
+				discription: 'Помпа – то механический насосос, надеваемый на горлышко бутылки. Закрепляется после сняти крышки. Не меняет температуру воды.',
 
 			},
 			{
 				title: 'Помпа BlueRain3',
 				price: 132,
+				rent: 20,
 				img: 'img/HandPomp.png',
-				label: 'new'
+				label: 'new',
+				discription: 'Помпа – то механический насосос, надеваемый на горлышко бутылки. Закрепляется после сняти крышки. Не меняет температуру воды.',
 			},
 			{
 				title: 'Помпа BlueRain4',
 				price: 133,
 				img: 'img/PompBox.jpg',
+				discription: 'Помпа – то механический насосос, надеваемый на горлышко бутылки. Закрепляется после сняти крышки. Не меняет температуру воды.',
 
 			}
 		],
@@ -88,40 +69,55 @@ new Vue({
 			{
 				title: 'Ecotronic H1-T (настольный)',
 				price: 130,
+				rent: 20,
 				img: 'img/Cooler.png',
-				label: 'mini'
+				label: 'mini',
+				discription: 'Кулер – то механический насосос, надеваемый на горлышко бутылки. Закрепляется после сняти крышки. Не меняет температуру воды.',
 			},
 			{
 				title: 'HotFrost D1150R',
 				price: 131,
 				img: 'img/cooler1.jpg',
+				discription: 'Кулер – то механический насосос, надеваемый на горлышко бутылки. Закрепляется после сняти крышки. Не меняет температуру воды.',
 
 			},
 			{
 				title: 'Помпа Ecotronic PICNIC',
 				price: 132,
+				rent: 20,
 				img: 'img/cooler2.jpg',
-				label: 'new'
+				label: 'new',
+				discription: 'Кулер – то механический насосос, надеваемый на горлышко бутылки. Закрепляется после сняти крышки. Не меняет температуру воды.',
 			},
 			{
 				title: 'Ecotronic H1-LN',
 				price: 133,
 				img: 'img/cooler3.jpg',
+				discription: 'Кулер – то механический насосос, надеваемый на горлышко бутылки. Закрепляется после сняти крышки. Не меняет температуру воды.',
 
 			},
 			{
 				title: 'Ecotronic H1-LNF',
 				price: 133,
+				rent: 20,
 				img: 'img/cooler4.jpg',
+				discription: 'Кулер – то механический насосос, надеваемый на горлышко бутылки. Закрепляется после сняти крышки. Не меняет температуру воды.',
 
 			}
 		],
-		title: "hello"
+		title: "hello",
+		showProduct: false,
+		modal: [],
 
 	},
 	methods: {
-		transition(e) {
-			console.log(e)
+		transition(pomp) {
+			this.showProduct = true
+			this.modal = pomp
+
+		},
+		closeProduct() {
+			this.showProduct = false
 		}
 	}
 });
