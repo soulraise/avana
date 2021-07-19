d996008cc0d93f6d133ddac10eafc68fdda65815
stavrdriff <stavr.driff@gmail.com>
2021-07-19 15:37:24 +0300

    added media for index

--- #src/js/alert.js
+++ #src/js/alert.js
@@ -22,12 +22,27 @@ function headerHidden() {
 		let pos = document.getElementById('anchor').getBoundingClientRect();
 		let posWindow2 = pos.y;
 		if (posWindow < posWindow2) {
-			document.querySelector('.header').classList.remove('header__fixed')
+			document.querySelector('.header').classList.remove('header__fixed');
+
 		} else {
 			document.querySelector('.header').classList.add('header__fixed')
+			document.getElementById('menu').classList.add('menu__hidden')
+			document.getElementById('burgerline1').classList.remove('burger__line_reversrotate45')
+			document.getElementById('burgerline2').classList.remove('burger__line_hidden')
+			document.getElementById('burgerline3').classList.remove('burger__line_rotate45')
 		}
 		posWindow = posWindow2;
 	})
 }
 headerHidden();
 
+function menuHidden() {
+
+	document.getElementById('burger').addEventListener('click', function () {
+		document.getElementById('menu').classList.toggle('menu__hidden')
+		document.getElementById('burgerline1').classList.toggle('burger__line_reversrotate45')
+		document.getElementById('burgerline2').classList.toggle('burger__line_hidden')
+		document.getElementById('burgerline3').classList.toggle('burger__line_rotate45')
+	})
+}
+menuHidden();
